137a741955987e91af88c0c51c634164fa45961e
BenedictWirtz <82606008+BenedictWirtz@users.noreply.github.com>
2025-01-31 12:47:22 +0100

    Selection of options

--- exord-challenge/src/DropDown.jsx
+++ exord-challenge/src/DropDown.jsx
@@ -1,12 +1,19 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
 
-// eslint-disable-next-line react/prop-types
+
 const DropDown = ({ placeholder, options }) => {
     const [isOpen, setIsOpen] = useState(false)
+    const [selected, setSelected] = useState(placeholder)
 
     console.log(isOpen)
     console.log(options)
+
+    const handleSelect = (option) => {
+        setSelected(option)
+        console.log("Selected:" + selected)
+        setIsOpen(false)
+    }
     
     return(
         <div>
@@ -15,7 +22,7 @@ const DropDown = ({ placeholder, options }) => {
             {isOpen && (
                 <ul>
                     {options.map((options, index) => (
-                        <li key={index}>
+                        <li key={index} onClick={() => handleSelect(options)}>
                             {options}
                         </li> 
                     ))}
